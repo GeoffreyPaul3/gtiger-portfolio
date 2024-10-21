fa98aa454bc3d5141657e76a5338b03f9a2fc2a1
GeoffreyPaul3 <geofreypaul40@gmail.com>
2024-10-21 23:07:42 +0200

    Updated Projects

--- app/(home)/components/Projects.tsx
+++ app/(home)/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SiNextdotjs, SiPostgresql, SiReact, SiReacthookform, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import { SiMongodb, SiNextdotjs, SiPostgresql, SiReact, SiReacthookform, SiShadcnui, SiTailwindcss, SiTypescript } from 'react-icons/si'
 import Title from './Title'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
@@ -16,24 +16,24 @@ const Projects = () => {
             background: "bg-indigo-500",
         },
         {
-            title: "Dream Employ - Simplify your job search with our cutting-edge platform",
-            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiPostgresql, SiReacthookform],
-            link: "https://www.dreamemploy.com",
-            cover: "/project-1.png",
+            title: "Filehugger - The greatest online tool for unlimited and free multimedia converting. ",
+            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiShadcnui],
+            link: "https://filehugger.vercel.app",
+            cover: "/project-2.png",
             background: "bg-green-500",
         },
         {
-            title: "Dream Employ - Simplify your job search with our cutting-edge platform",
-            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiPostgresql, SiReacthookform],
-            link: "https://www.dreamemploy.com",
-            cover: "/project-1.png",
+            title: "TPay - Easily exchange currencies and make payments with T Pay. Secure and reliable services for all your financial needs.",
+            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss,SiShadcnui, SiMongodb, SiReacthookform],
+            link: "https://t-pay.vercel.app",
+            cover: "/project-3.png",
             background: "bg-green-500",
         },
         {
-            title: "Dream Employ - Simplify your job search with our cutting-edge platform",
-            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, SiPostgresql, SiReacthookform],
-            link: "https://www.dreamemploy.com",
-            cover: "/project-1.png",
+            title: "Music in the Hood - Your go-to destination for discovering the next big stars in the music industry.",
+            tech: [SiNextdotjs, SiReact, SiTypescript, SiTailwindcss, ],
+            link: "https://music-inthe-hood.vercel.app",
+            cover: "/project-4.png",
             background: "bg-indigo-500",
         },
     ]
